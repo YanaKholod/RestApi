eb38ee0b949929c1f37796f74642a1a700e0e786
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-07-10 16:15:16 +0300

    server

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+// то что пользуется базами
+
+const mongoose = require("mongoose"); // подключаемся к базе
 const app = require("./app");
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// const { DB_HOST } = require("./config"); // подключение с конфиг файла
+const { DB_HOST } = process.env; // импорт с переменных окружения
+require("dotenv").config();
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000); // подключаемся к базе и потом только запускаем сервер
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1); // закрывает все фонновые процессы
+  });
 
 // pass: jEeLwmQRFvrdqYjv
